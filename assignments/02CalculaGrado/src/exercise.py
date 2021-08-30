36b3747e1be40bffafe8f21199635e7d9cb3209c
A01254123 <A01254123@itesm.mx>
2021-08-30 21:52:26 +0000

    CesarMtz_calculagrado

--- assignments/02CalculaGrado/src/exercise.py
+++ assignments/02CalculaGrado/src/exercise.py
@@ -1,7 +1,20 @@
-def main():
+def calcula_grado(numero):
+    if numero<=1 and numero>=0:
+        if numero>0.9:
+            return "A" 
+        elif numero>0.8:
+            return "B"
+        elif numero>0.7:
+            return "C"  
+        elif numero>0.6:
+            return "D"  
+        elif numero<0.6:
+            return "F"
+    else:
+        return "score incorrecto"  
+def main(): 
     #escribe tu código abajo de esta línea
-    x = float(input("Ingresa Un valor entre 0.0 y 1.0: "))
+    x=float(input("Ingresa Un valor entre 0.0 y 1.0: "))
     print(calcula_grado(x))
-
 if __name__=='__main__':
     main()
